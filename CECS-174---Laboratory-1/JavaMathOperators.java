49227ed5b7f986ff5bcad344159159ad56a80c63
Jimi Som <jimysom@yahoo.com>
2013-10-03 14:59:46 -0700

    Create JavaMathOperators.java

---
+++ CECS-174---Laboratory-1/JavaMathOperators.java
@@ -0,0 +1,11 @@
+import javax.swing.*;// package
+public class JavaMathOperators
+{
+public static void main(String[] args)
+ { 
+  System.out.print("Look 1/3 = "+1/3+", but 1.0/3 = "+1.0/3);
+  System.out.println(" Also 1/2 = "+1/2+", but 1/2.0 = "+1/2.0);
+  System.out.print("Mod % means print the remainder, 15%4 = "+15%4);
+  System.out.println(" Finally, 30/4 = "+30/4+" which is only the quotient.");
+ }//end main
+}//end Class JavaMathOperators
