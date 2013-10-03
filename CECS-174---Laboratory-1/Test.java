e12178f02f896efce485bb22010ad9c07a92e008
Jimi Som <jimysom@yahoo.com>
2013-10-03 15:20:29 -0700

    Problem 1
    Updated

---
+++ CECS-174---Laboratory-1/Test.java
@@ -0,0 +1,15 @@
+import javax.swing.*; //for gui window
+import java.text.*;
+public class Test { 
+	/**CECS 174
+	 * Johnjimy Som
+	 * Instructor: Konig
+	 */
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	System.out.println(3+4);
+System.out.println("The sum of 3+5 = "+3+5+" Which is incorrect....");
+	System.out.print("but (3+5) works = "+(3+5));
+	}//end of main
+} //end of pr0gram (Class Test)
+// Problem 1
