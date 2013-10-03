ef963713f134dae41038f0551049364b896919c3
Jimi Som <jimysom@yahoo.com>
2013-10-03 15:24:43 -0700

    Problem 2
    Simple GUI window.

---
+++ CECS-174---Laboratory-1/WindowSample.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class WindowSample 
+{
+ public static void main(String[] args) { 
+  JFrame myWindow;
+  myWindow = new JFrame();
+  myWindow.setSize(300,200);
+  myWindow.setTitle("My Very First Java Window!!");
+  myWindow.setVisible(true);
+ }//end main
+}//end Class WindowSample
+//Problem 2
+// A simple gui window
