8313e041d57a59bd325df97508008dbfab205b52
Jimi Som <jimysom@yahoo.com>
2013-10-03 15:11:42 -0700

    Create ForLoopExample.java

---
+++ CECS-174---Laboratory-1/ForLoopExample.java
@@ -0,0 +1,15 @@
+import java.text.*;
+public class ForLoopExample {
+public static void main(String[] args){
+ //Lets print out the first 10 even numbers, starting with 2
+  int i;
+  for(i=1;i<=10;i++)
+   System.out.print(2*i+", ");
+  System.out.println();//go to the next line
+  //Now lets do the same a different way and then clean up the last ","
+  for(i=2;i<20;i=i+2)  {
+	  System.out.print(i+", ");
+	  }//end of for loop, now print the last number
+	  System.out.println(i);
+	 }
+	}
