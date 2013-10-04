806d67779ea3eae81f380fd517e6ee9edc79b15f
Jimi Som <jimysom@yahoo.com>
2013-10-03 19:01:21 -0700

    Problem 8
    Simple While loop

---
+++ CECS-174---Laboratory-1/JavaWhileLoop.java
@@ -0,0 +1,10 @@
+import java.text.*;
+public class JavaWhileLoop{
+public static void main(String[] args){
+double a=500.0;
+while(a>10.0){
+  System.out.print(a+"    ");
+  a=a/2.0;
+}//end while loop
+}//end Main
+}//end Class JavaWhileLoop
