66750e0d4ac28f6afc0a6dc4a6d0af5771405a5d
Jimi Som <jimysom@yahoo.com>
2013-11-08 10:01:49 -0800

    Problem 1B

---
+++ CECS-174---Laboratory-4/Authority.java
@@ -0,0 +1,20 @@
+//Problem 1B Authority
+import java.awt.*;//Packages
+import javax.swing.*;
+public class Authority
+{
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame ("Authority");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel primary = new JPanel();
+		primary.setBackground(Color.yellow);
+		primary.setPreferredSize(new Dimension(250,75));
+		JLabel label1 = new JLabel("Question authority and");
+		JLabel label2 = new JLabel("the moral majority is neither");
+		primary.add(label1);
+		primary.add(label2);
+		frame.getContentPane().add(primary);
+		frame.pack();frame.setVisible(true);
+	}//end public main
+}//end classs Authority
